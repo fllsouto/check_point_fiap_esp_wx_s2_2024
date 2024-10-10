508b0eb01ac9d42190a62f690e0cb33d12fedcb3
Fabr1c100 <rm97631@fiap.com.br>
2024-10-09 22:27:57 -0300

    Atualização do grupo no App.java

--- checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
+++ checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
@@ -3,12 +3,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Grupo: Batata Doce
+ * Grupo: Milho
  * Autores: 
- *      - João da Silva     -   jaodasilva@example.com
- *      - Jose da Silva     -   zedasilva@example.com
- *      - Maria da Silva    -   marydasilva@example.com
- *      - Monica da Silva   -   nicadasilva@example.com
+ *          Fabrício Saavedra   -   rm97631@fiap.com.br
+            Guilherme Akio      -   rm98582@fiap.com.br
+            Matheus Motta       -   rm550352@fiap.com.br
  */
 
 package br.com.fiap.twoespwx.libunclepresser;
@@ -18,6 +17,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("LIB UNCLE PRESSER - GRUPO BATATA-DOCE");
+        System.out.println("LIB UNCLE PRESSER - GRUPO MILHO");
     }
 }
